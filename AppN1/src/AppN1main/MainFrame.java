e016bed7998229cf4404d15e011a10c60c14a82f
Anton <1@1.com>
2013-06-15 02:32:14 +0600

    qwerty

---
+++ AppN1/src/AppN1main/MainFrame.java
@@ -0,0 +1,63 @@
+package AppN1main;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainFrame {
+
+	private JFrame frame;
+	private JTextArea textArea;
+	private JButton btnNewButton;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame window = new MainFrame();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainFrame() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		
+		btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if ((textArea.getText()).equals("begin"))
+					JOptionPane.showMessageDialog(frame, "Message text");
+			}
+		});
+		panel.add(btnNewButton);
+		
+		textArea = new JTextArea();
+		frame.getContentPane().add(textArea, BorderLayout.CENTER);
+	}
+
+}
